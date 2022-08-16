0f09ab54123ddd3387ef73ed30d544dc627fae28
dhlarrotar <91299939+dhlarrotar@users.noreply.github.com>
2022-08-16 11:46:40 -0500

    add loader

---
+++ js/loader.js
@@ -0,0 +1,10 @@
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("main").style.visibility = "hidden";
+    document.querySelector(".loader").style.visibility = "visible";
+  } else {
+    document.querySelector(".loader").style.display = "none";
+    document.querySelector("main").style.visibility = "visible";
+  }
+};
